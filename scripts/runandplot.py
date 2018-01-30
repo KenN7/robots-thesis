90f7a888d27a55ffe6d1654f52608c1fb14d424a
Ken <ken@kenh.fr>
2018-01-30 18:06:07 +0100

    heavy mod of runandplot

--- scripts/runandplot.py
+++ scripts/runandplot.py
@@ -24,23 +24,35 @@ import shlex
 
 CONF = {
     "Aggregation":
-        ( "automodegianduja_aggregation.xml", "/home/ken/depots/neat-argos3/optimization/expAGG/results-evo-agg227", "/home/ken/depots/argos3-Automode/optimization/aggreg-200k-0208exp-results.txt" ),
+        (
+            "automodegianduja_aggregation.xml", ("/home/khasselmann/neat-argos3/optimization/expAGG", 'evo', 'agg227' ),
+            ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'aggreg')
+            ("/home/ken/depots/argos3-Automode/optimization/aggreg-200k-0208exp-results.txt", 'auto', 'stopnogian1801')
+        ),
     "Decision making":
-        ( "automodegianduja_decision.xml",
-        "/home/ken/depots/neat-argos3/optimization/expDEC/results-evo-dec227",
-        "/home/ken/depots/argos3-Automode/optimization/decision-results.txt" ),
+        (
+            "automodegianduja_decision.xml",
+            ("/home/khasselmann/neat-argos3/optimization/expDEC", 'evo', 'dec227'),
+            ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'decision')
+            ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'desinogian1801')
+        ),
     "Stop":
-        ( "automodegianduja_stop.xml",
-        "/home/ken/depots/neat-argos3/optimization/expDEC/results-evo-dec227",
-        "/home/ken/depots/argos3-Automode/optimization/stop-results.txt" ),
-    "Aggregation 2 Spots":
-        ( "automodegianduja_aggregation_2spots.xml",
-        "/home/ken/depots/neat-argos3/optimization/expAGG2/results-evo-agg298",
-        "/home/ken/depots/argos3-Automode/optimization/aggreg2spotsng-200k-exp-results.txt" )
+        (
+            "automodegianduja_stop.xml",
+            ("/home/khasselmann/neat-argos3/optimization/expSTOP", 'evo', 'stop227'),
+            ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'stop')
+            ("/home/ken/depots/argos3-Automode/optimization/stop-results.txt", 'auto', 'stopREALnogian1801')
+        ),
+    # "Aggregation 2 Spots":
+    #     ( "automodegianduja_aggregation_2spots.xml",
+    #     "/home/ken/depots/neat-argos3/optimization/expAGG2/results-evo-agg298",
+    #     "/home/ken/depots/argos3-Automode/optimization/aggreg2spotsng-200k-exp-results.txt" )
     }
 
 NEATFOLDER = "/home/ken/depots/neat-argos3"
 AUTOFOLDER = "/home/ken/depots/argos3-Automode"
+
+METHODS = ('Evostick', 'AutoMoDe-Gianduja', 'AutoMoDe-Chocolate')
 
 SEEDS = [77622,72184,63036,4377,67310,8329,19553,70157,97793,45059,87549,44650,56617,83729,17988,80308,74870,81425,73849,7612,74525,6082,5535,18892,54109,62956,70654,30093,59206,66078,57389,29920,41911,34996,32772,18813,15631,44994,67962,43963,62189,65015,20277,20892,94474,42929,87693,98128,78246,5081,51028,28832,37383,34385,96043,95415,16672,9221,14671,79820,7583,58134,64049,7548,19890,46885,52388,87754,47343,46009,17254,39786,12071,82478,51590,90271,21440,88504,10126,72952,57303,87190,74278,58122,69996,62778,66277,61464,43039,92123,77088,52577,70407,69179,82028,45716,9989,39975,45670,35842,85831,96278,80536,87372,99892,89604,83114,6497,92972,11535,92749,82858,15932,61397,78113,30010,48677,40027,55937,79284,37609,10327,42676,33507,84383,21087,12499,24034,88644,23820,71629,7732,19319,3365,16774,31355,7638,8528,36091,58938,66126,22108,33965,97046,48792,62804,68988,26587,54961,2954,47708,34028,36482,81391,99726,26518,72623,80215,89653,87266,10839,27026,5795,6481,648254,87124,11627,62506,51785,36911,6679,8832,564211,94393,9615,5320,259204,5515,8628,565757,72321,13478,16519,4686,43032,34280,84357,55124,93746,93670,85757,49975,28413,88216,95239,21483,32900,84008,57612,60900]
 
@@ -77,17 +89,15 @@ def auto(seed, statem, xml):
         print(e)
 
 
-def write_res(evo, auto, task):
+def write_res(scores, task):
     print('opening score file')
     f = open('scores-%s.txt' % task, 'w')
     f.write('"res" "alg" "task"\n')
     i = 0
-    for res in evo:
-        f.write('"%i" "%s" "Evostick" "%s"\n' % (i,res,task))
-        i+=1
-    for res in auto:
-        f.write('"%i" "%s" "AutoMode-Gianduja" "%s"\n' % (i,res,task))
-        i+=1
+    for method in scores:
+        for i,res in enumerate(method):
+            f.write('"%i" "%s" "%s" "%s"\n' % (i,res,METHODS[i],task))
+            i+=1
     print('finished writing score file..')
 
 
@@ -99,24 +109,25 @@ def run_R(filen, title):
     pro = subprocess.run(c, stdout=subprocess.PIPE)
 
 
-#new stuff --------------
-def extract_res_evo(name):
+def extract_res_evo(name, folder):
     t = re.compile('%s-(\d+)' % name)
-    os.mkdir('results-evo-%s' % name)
-    for i in os.listdir():
+    os.makedirs('temp/results-evo-%s' % name)
+    for i in os.listdir(folder):
         if t.match(i):
             y = t.match(i)
             print(i)
-            shutil.copyfile( i+"/gen/gen_last_1_champ",'results-evo-%s/gen_champ_%s' % (sys.argv[1],y.group(1)) )
+            shutil.copyfile( i+"/gen/gen_last_1_champ",'temp/results-evo-%s/gen_champ_%s' % (sys.argv[1],y.group(1)) )
+    return os.path.abspath('temp/results-evo-%s' % name)
 
 
 
-def extract_res_auto(name):
+def extract_res_auto(name, folder):
     t = re.compile('%s-200k-exp-(\d\d)' % name)
     m = re.compile(r"# Best configurations as commandlines \(first number is the configuration ID\)\n\d*  (.*)")
 
-    f2 = open('%s-results.txt' % name,'w')
-    for i in os.listdir():
+    os.mkdir('temp')
+    f2 = open('temp/%s-results.txt' % name,'w')
+    for i in os.listdir(folder):
         if t.match(i):
             print(i)
             y = t.match(i)
@@ -128,35 +139,83 @@ def extract_res_auto(name):
             f2.write('\n')
             f.close()
             #print(y.group(1))
-
     f2.close()
-#new end --------------
+    return os.path.abspath('temp/%s-results.txt' % name)
 
 
-def main(expfile, genfolder, autopfsm, task):
+def main(task):
+    # expfile, genfolder, autopfsm
     #evo
     #args = p.parse_args()
-    xml = os.path.join(NEATFOLDER,'experiments',expfile)
-    g = os.listdir(genfolder)
-    scoreevo = []
-    for i,l in enumerate(g):
-        sc = evo(SEEDS[i],os.path.join(genfolder,l),xml)
-        scoreevo.append(sc)
-        print(sc)
 
-    #auto
-    xml = os.path.join(AUTOFOLDER,'experiments',expfile)
-    f = open(autopfsm)
-    scoreauto = []
-    for i,l in enumerate(f):
-        sc = auto(SEEDS[i],l.strip(),xml)
-        scoreauto.append(sc)
-        print(sc)
-    write_res(scoreevo, scoreauto, task)
+    #scp:
+    #make dir temp
+
+
+    scores = []
+
+    expfile = CONF[task][0]
+    for method in CONF[task][1:]:
+        if method[1] == 'evo':
+
+            scpcom = "scp -r khasselmann@majorana.ulb.ac.be:%s temp/" % method[0]
+            command = shlex.split(command)
+            print("running %s" % command)
+            pro = subprocess.run(command, stdout=subprocess.PIPE)
+            #os.mkdir
+            direxp = extract_res_evo(method[2],os.path.join('temp',os.path.basename(method[0])))
+
+            xml = os.path.join(NEATFOLDER,'experiments',expfile)
+            g = os.listdir(direxp)
+            scoresevo = []
+            for i,l in enumerate(g):
+                sc = evo(SEEDS[i],os.path.join(direxp,l),xml)
+                scoresevo.append(sc)
+                print(sc)
+            scores.append(scoresevo)
+
+        else if method[1] == "auto":
+
+            scpcom = """rsync -rav -e ssh --include='*.stdout' khasselmann@majorana.ulb.ac.be:%s temp/""" % method[0]
+            command = shlex.split(command)
+            print("running %s" % command)
+            pro = subprocess.run(command, stdout=subprocess.PIPE)
+            print(pro.stdout)
+            #os.mkdir
+            autopfsm = extract_res_auto(method[2],os.path.join('temp',os.path.basename(method[0])))
+
+            xml = os.path.join(AUTOFOLDER,'experiments',expfile)
+            f = open(autopfsm)
+            scoresauto = []
+            for i,l in enumerate(f):
+                sc = auto(SEEDS[i],l.strip(),xml)
+                scoresauto.append(sc)
+                print(sc)
+            f.close()
+            scores.append(scoresauto)
+
+    # xml = os.path.join(NEATFOLDER,'experiments',expfile)
+    # g = os.listdir(genfolder)
+    # scoreevo = []
+    # for i,l in enumerate(g):
+    #     sc = evo(SEEDS[i],os.path.join(genfolder,l),xml)
+    #     scoreevo.append(sc)
+    #     print(sc)
+    #
+    # #auto
+    # xml = os.path.join(AUTOFOLDER,'experiments',expfile)
+    # f = open(autopfsm)
+    # scoreauto = []
+    # for i,l in enumerate(f):
+    #     sc = auto(SEEDS[i],l.strip(),xml)
+    #     scoreauto.append(sc)
+    #     print(sc)
+
+    write_res(scores, task)
     run_R('scores-%s.txt' % task, task)
 
 
 if __name__ == "__main__":
     for item in CONF:
         print(item)
-        main(CONF[item][0],CONF[item][1],CONF[item][2],item)
+        main(item)
