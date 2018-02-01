c06ec1be1bca9a13ca1a860d5b6f66a527bcab24
Ken <ken@kenh.fr>
2018-02-01 17:51:41 +0100

    add skip get option

--- scripts/runandplot.py
+++ scripts/runandplot.py
@@ -8,27 +8,31 @@ import shlex
 import shutil
 from paramiko import SSHClient
 
+p = argparse.ArgumentParser(description='getter, runner and grapher of results, options are choosed by editting the python file.')
+
+p.add_argument('-s', '--skipget', action='store_true', help="if present skips getting the results from majorana and just runs based of content of temp/ folder", required=False)
+
 
 CONF = {
-    # "Aggregation":
-    #     (
-    #         "automodegianduja_aggregation.xml", ("/home/khasselmann/neat-argos3/optimization/expAGG", 'evo', 'agg227' ),
-    #         ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'aggreg'),
-    #         ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'aggnogian3101')
-    #     ),
-    # "Decision making":
-    #     (
-    #         "automodegianduja_decision.xml",
-    #         ("/home/khasselmann/neat-argos3/optimization/expDEC", 'evo', 'dec227'),
-    #         ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'decision'),
-    #         ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'desinogian3101')
-    #     ),
+    "Aggregation":
+        (
+            "automodegianduja_aggregation.xml", ("/home/khasselmann/neat-argos3/optimization/expAGG", 'evo', 'agg227' ),
+            ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'aggreg'),
+            #("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'aggnogian3101')
+        ),
+    "Decision making":
+        (
+            "automodegianduja_decision.xml",
+            ("/home/khasselmann/neat-argos3/optimization/expDEC", 'evo', 'dec227'),
+            ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'decision'),
+            #("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'desinogian3101')
+        ),
     "Stop":
         (
             "automodegianduja_stop.xml",
             ("/home/khasselmann/neat-argos3/optimization/expSTOP", 'evo', 'stop227'),
             ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'stop'),
-            ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'stopnogian3101')
+            #("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'stopnogian3101')
         ),
     # "Aggregation 2 Spots":
     #     ( "automodegianduja_aggregation_2spots.xml",
@@ -41,7 +45,7 @@ AUTOFOLDER = "/home/ken/depots/argos3-Automode"
 
 METHODS = ('Evostick', 'AutoMoDe-Gianduja', 'AutoMoDe-Chocolate')
 
-SEEDS = [77622,72184,63036,4377,67310,8329,19553,70157,97793,45059,87549,44650,56617,83729,17988,80308,74870,81425,73849,7612,74525,6082,5535,18892,54109,62956,70654,30093,59206,66078,57389,29920,41911,34996,32772,18813,15631,44994,67962,43963,62189,65015,20277,20892,94474,42929,87693,98128,78246,5081,51028,28832,37383,34385,96043,95415,16672,9221,14671,79820,7583,58134,64049,7548,19890,46885,52388,87754,47343,46009,17254,39786,12071,82478,51590,90271,21440,88504,10126,72952,57303,87190,74278,58122,69996,62778,66277,61464,43039,92123,77088,52577,70407,69179,82028,45716,9989,39975,45670,35842,85831,96278,80536,87372,99892,89604,83114,6497,92972,11535,92749,82858,15932,61397,78113,30010,48677,40027,55937,79284,37609,10327,42676,33507,84383,21087,12499,24034,88644,23820,71629,7732,19319,3365,16774,31355,7638,8528,36091,58938,66126,22108,33965,97046,48792,62804,68988,26587,54961,2954,47708,34028,36482,81391,99726,26518,72623,80215,89653,87266,10839,27026,5795,6481,648254,87124,11627,62506,51785,36911,6679,8832,564211,94393,9615,5320,259204,5515,8628,565757,72321,13478,16519,4686,43032,34280,84357,55124,93746,93670,85757,49975,28413,88216,95239,21483,32900,84008,57612,60900]
+SEEDS = [98128,78246,5081,51028,28832,37383,34385,96043,95415,16672,9221,14671,79820,7583,58134,64049,7548,19890,46885,52388,87754,47343,46009,17254,39786,12071,82478,51590,90271,21440,88504,10126,72952,57303,87190,74278,58122,69996,62778,66277,61464,43039,92123,77088,52577,70407,69179,82028,45716,9989,39975,45670,35842,85831,96278,80536,87372,99892,89604,83114,6497,92972,11535,92749,82858,15932,61397,78113,30010,48677,40027,55937,79284,37609,10327,42676,33507,84383,21087,12499,24034,88644,23820,71629,7732,19319,3365,16774,31355,7638,8528,36091,58938,66126,22108,33965,97046,48792,62804,68988,26587,54961,2954,47708,34028,36482,81391,99726,26518,72623,80215,89653,87266,10839,27026,5795,6481,648254,87124,11627,62506,51785,36911,6679,8832,564211,94393,9615,5320,259204,5515,8628,565757,72321,13478,16519,4686,43032,34280,84357,55124,93746,93670,85757,49975,28413,88216,95239,21483,32900,84008,57612,60900,77622,72184,63036,4377,67310,8329,19553,70157,97793,45059,87549,44650,56617,83729,17988,80308,74870,81425,73849,7612,74525,6082,5535,18892,54109,62956,70654,30093,59206,66078,57389,29920,41911,34996,32772,18813,15631,44994,67962,43963,62189,65015,20277,20892,94474,42929,87693]
 
 
 def evo(seed, genome, xml):
@@ -112,7 +116,6 @@ def extract_res_evo(name, folder):
     return os.path.abspath('temp/results-evo-%s' % name)
 
 
-
 def extract_res_auto(name, remotefolder, sftp):
     t = re.compile('%s-200k-exp-(\d\d)' % name)
     m = re.compile(r"# Best configurations as commandlines \(first number is the configuration ID\)\n\d*  (.*)")
@@ -140,57 +143,82 @@ def extract_res_auto(name, remotefolder, sftp):
 def main(task):
     # expfile, genfolder, autopfsm
     #evo
-    #args = p.parse_args()
+    args = p.parse_args()
 
     scores = []
 
     expfile = CONF[task][0]
     for method in CONF[task][1:]:
-        if method[1] == 'evo':
+        if (args.skipget == False):
+            if method[1] == 'evo':
+                scpcom = "scp -r khasselmann@majorana.ulb.ac.be:%s/* temp/" % method[0]
+                command = shlex.split(scpcom)
+                print("running %s" % command)
+                pro = subprocess.run(command, stdout=subprocess.PIPE)
+                print(pro.stdout)
+                #os.mkdir
+                direxp = extract_res_evo(method[2],'temp/')
 
-            scpcom = "scp -r khasselmann@majorana.ulb.ac.be:%s/* temp/" % method[0]
-            command = shlex.split(scpcom)
-            print("running %s" % command)
-            pro = subprocess.run(command, stdout=subprocess.PIPE)
-            print(pro.stdout)
-            #os.mkdir
-            direxp = extract_res_evo(method[2],'temp/')
+                xml = os.path.join(NEATFOLDER,'experiments',expfile)
+                g = os.listdir(direxp)
+                scoresevo = []
+                for i,l in enumerate(g):
+                    sc = evo(SEEDS[i],os.path.join(direxp,l),xml)
+                    scoresevo.append(sc)
+                    print(sc)
+                scores.append(scoresevo)
 
-            xml = os.path.join(NEATFOLDER,'experiments',expfile)
-            g = os.listdir(direxp)
-            scoresevo = []
-            for i,l in enumerate(g):
-                sc = evo(SEEDS[i],os.path.join(direxp,l),xml)
-                scoresevo.append(sc)
-                print(sc)
-            scores.append(scoresevo)
+            elif method[1] == "auto":
 
-        elif method[1] == "auto":
+                #scpcom = """rsync -rav -e ssh --include='' --include='*.stdout' khasselmann@majorana.ulb.ac.be:%s temp/""" % method[0]
+                #command = shlex.split(command)
+                #print("running %s" % command)
+                #pro = subprocess.run(command, stdout=subprocess.PIPE)
+                #print(pro.stdout)
+                client = SSHClient()
+                client.load_system_host_keys()
+                client.connect('majorana.ulb.ac.be', username='khasselmann')
+                sftp = client.open_sftp()
+                #os.mkdir
+                autopfsm = extract_res_auto(method[2],method[0],sftp)
 
-            #scpcom = """rsync -rav -e ssh --include='' --include='*.stdout' khasselmann@majorana.ulb.ac.be:%s temp/""" % method[0]
-            #command = shlex.split(command)
-            #print("running %s" % command)
-            #pro = subprocess.run(command, stdout=subprocess.PIPE)
-            #print(pro.stdout)
-            client = SSHClient()
-            client.load_system_host_keys()
-            client.connect('majorana.ulb.ac.be', username='khasselmann')
-            sftp = client.open_sftp()
-            #os.mkdir
-            autopfsm = extract_res_auto(method[2],method[0],sftp)
+                client.close()
 
-            client.close()
+                xml = os.path.join(AUTOFOLDER,'experiments',expfile)
+                f = open(autopfsm)
+                scoresauto = []
+                for i,l in enumerate(f):
+                    sc = auto(SEEDS[i],l.strip(),xml)
+                    scoresauto.append(sc)
+                    print(sc)
+                f.close()
+                scores.append(scoresauto)
 
-            xml = os.path.join(AUTOFOLDER,'experiments',expfile)
-            f = open(autopfsm)
-            scoresauto = []
-            for i,l in enumerate(f):
-                sc = auto(SEEDS[i],l.strip(),xml)
-                scoresauto.append(sc)
-                print(sc)
-            f.close()
-            scores.append(scoresauto)
+        elif (args.skipget == True):
+            if method[1] == 'evo':
+                direxp = extract_res_evo(method[2],'temp/')
 
+                xml = os.path.join(NEATFOLDER,'experiments',expfile)
+                g = os.listdir(direxp)
+                scoresevo = []
+                for i,l in enumerate(g):
+                    sc = evo(SEEDS[i],os.path.join(direxp,l),xml)
+                    scoresevo.append(sc)
+                    print(sc)
+                scores.append(scoresevo)
+
+            elif method[1] == "auto":
+                autopfsm = os.path.abspath('temp/%s-results.txt' % method[2])
+
+                xml = os.path.join(AUTOFOLDER,'experiments',expfile)
+                f = open(autopfsm)
+                scoresauto = []
+                for i,l in enumerate(f):
+                    sc = auto(SEEDS[i],l.strip(),xml)
+                    scoresauto.append(sc)
+                    print(sc)
+                f.close()
+                scores.append(scoresauto)
 
     # xml = os.path.join(NEATFOLDER,'experiments',expfile)
     # g = os.listdir(genfolder)
