113b445f94b3a00eadde246fc468a7cd30b6e285
Ken <ken@kenh.fr>
2018-04-15 11:55:37 +0200

    gen res

--- Robot-experiments/testRew.R
+++ Robot-experiments/testRew.R
@@ -24,36 +24,36 @@ generateplot <- function(datareal, datasim, task, max) {
   ecdf2s = ecdf(evosim)
   ecdf3s = ecdf(chocosim)
 
-  saving_path <- file.path(".", paste(task, "-ECDFReal.tex", sep=""))
-  tikz(file = saving_path, width=pdf.dim[1], height=pdf.dim[2])
-  #jpeg(file=saving_path)
+  #saving_path <- file.path(".", paste(task, "-ECDFReal.tex", sep=""))
+  #tikz(file = saving_path, width=pdf.dim[1], height=pdf.dim[2])
+  ##jpeg(file=saving_path)
 
-  par(cex.axis=1.5, cex.lab=1.5, cex.main=1.9)
-  plot(ecdf1r, verticals=TRUE, do.points=FALSE,
-       xlim = c(0,max), xlab= 'Objective function',
-       ylab='',
-       main = 'Empirical Cumulative Distribution',
-       lty = "dotted"
-       )
-  plot(ecdf2r, verticals=TRUE, do.points=FALSE, add=TRUE, lty = "dashed")
-  plot(ecdf3r, verticals=TRUE, do.points=FALSE, add=TRUE)
-  legend(0, 1.0, legend = c("Chocolate","EvoCom","Gianduja"), lty=1:3 )
-  dev.off()
+  #par(cex.axis=1.5, cex.lab=1.5, cex.main=1.9)
+  #plot(ecdf1r, verticals=TRUE, do.points=FALSE,
+  #     xlim = c(0,max), xlab= 'Objective function',
+  #     ylab='',
+  #     main = 'Empirical Cumulative Distribution',
+  #     lty = "dotted"
+  #     )
+  #plot(ecdf2r, verticals=TRUE, do.points=FALSE, add=TRUE, lty = "dashed")
+  #plot(ecdf3r, verticals=TRUE, do.points=FALSE, add=TRUE)
+  #legend(0, 1.0, legend = c("Chocolate","EvoCom","Gianduja"), lty=1:3 )
+  #dev.off()
 
 
-  saving_path2 <- file.path(".", paste(task, "-ECDFSim.tex", sep=""))
-  tikz(file = saving_path2, width=pdf.dim[1], height=pdf.dim[2])
-  #jpeg(file=saving_path2)
+  #saving_path2 <- file.path(".", paste(task, "-ECDFSim.tex", sep=""))
+  #tikz(file = saving_path2, width=pdf.dim[1], height=pdf.dim[2])
+  ##jpeg(file=saving_path2)
 
-  par(cex.axis=1.5, cex.lab=1.5, cex.main=1.9)
-  plot(ecdf1s, verticals=TRUE, do.points=FALSE,
-       xlim = c(0,max), xlab= 'Objective function',
-       ylab='',
-       main = 'Empirical Cumulative Distribution'
-       )
-  plot(ecdf2s, verticals=TRUE, do.points=FALSE, add=TRUE, col='brown')
-  plot(ecdf3s, verticals=TRUE, do.points=FALSE, add=TRUE, col='orange')
-  dev.off()
+  #par(cex.axis=1.5, cex.lab=1.5, cex.main=1.9)
+  #plot(ecdf1s, verticals=TRUE, do.points=FALSE,
+  #     xlim = c(0,max), xlab= 'Objective function',
+  #     ylab='',
+  #     main = 'Empirical Cumulative Distribution'
+  #     )
+  #plot(ecdf2s, verticals=TRUE, do.points=FALSE, add=TRUE, col='brown')
+  #plot(ecdf3s, verticals=TRUE, do.points=FALSE, add=TRUE, col='orange')
+  #dev.off()
 
   list_methods <- c("GianS", "GianR", "EvoS", "EvoR", "ChocoS", "ChocoR")
 
@@ -92,7 +92,7 @@ print("Boxplots ...")
 pdf.dim <- c(5,6) # width, height
 
 
-datasim = read.csv("scores-Decision Making.txt")
+datasim = read.csv("scores-Decision.txt")
 datareal = read.csv("real-scores-deci.txt")
 
 generateplot(datareal, datasim, "decision", 24000)
