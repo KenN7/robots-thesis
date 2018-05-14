033fa76a6f37c4778eebfa52ad042c7fbab3bc4f
Ken <ken@kenh.fr>
2018-05-14 14:56:33 +0200

    extractFSM

---
+++ scripts/extractFSM.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import urllib.request
+import sys
+
+def main():
+    urls = [x for x in sys.argv[1:]]
+    print(urls)
+    for i,url in enumerate(urls[:-1]):
+        urllib.request.urlretrieve(url, "fsm{:02d}.png".format(i+1))
+
+
+if __name__ == '__main__':
+    main()
