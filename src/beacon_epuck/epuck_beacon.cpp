8241d0364e454c18b066be26fdd9b8ee6eb7e8f0
Ken <ken@kenh.fr>
2018-09-18 12:33:28 +0200

    beaconbug fix

--- src/beacon_epuck/epuck_beacon.cpp
+++ src/beacon_epuck/epuck_beacon.cpp
@@ -79,7 +79,7 @@ void CEPuckBeacon::Init(TConfigurationNode& t_node) {
     * have to recompile if we want to try other settings.
     */
     GetNodeAttributeOrDefault(t_node, "velocity", m_fWheelVelocity, m_fWheelVelocity);
-    GetNodeAttributeOrDefault(t_node, "time", m_unTBar, (UInt32) 0 );
+    GetNodeAttributeOrDefault(t_node, "time", m_unTBarParam, (SInt32) 0 );
     if (m_unTBarParam == -1) {
         m_unTBar = m_pcRng->Uniform(CRange<UInt32>(0, 900));
     }
@@ -159,6 +159,7 @@ void CEPuckBeacon::ControlStep() {
     /* Get the highest reading in front of the robot, which corresponds to the closest object */
     if (m_unTimeStep > m_unTBar && m_unState == 0) {
         SendBeacon(m_unMessageToSend);
+        LOG << "becmes: " << m_unMessageToSend << std::endl;
         m_unState = 1;
     }
     // if (m_pcGroundSensor != NULL) {
