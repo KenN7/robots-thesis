5392b6db8632f7916af473ee99b10c8406ad8f8e
Ken <ken@kenh.fr>
2019-09-27 16:23:47 +0200

    get rsults

--- scripts/getresults.py
+++ scripts/getresults.py
@@ -30,61 +30,78 @@ CONF = {
     #         ("/home/khasselmann/temp", 'auto', 'stop1.3'),
     #         ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'stop1.2')
     #     ),
-    "BeacAgg": # bagg2.0d-new-exp-01  bagg3.0d-new-exp-01  bagg2.1d-new-exp-01 bagg3.1d-new-exp-01
-        (
+    #"BeacAgg": # bagg2.0d-new-exp-01  bagg3.0d-new-exp-01  bagg2.1d-new-exp-01 bagg3.1d-new-exp-01
+    #    (
 
-            ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'bagg6_6_2.1d-240-nlongRange'),
-            # ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'beacagg3.0dev'),
-            # ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'beacagg3.1dev'),
-            # ("/home/khasselmann/neat-argos3/optimization/expBAG3", 'evo', 'beacagg3'),
-            # ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'bagg3.0d-new'),
-            # ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'bagg2.0d-new'),
-            # ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'bagg2.1d-new'),
-            # ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'bagg3.1d-new'),
-            # ("/home/khasselmann/neat-argos3/optimization/expBAG2-2N", 'evo', 'beacagg2-new'),
-            # ("/home/khasselmann/neat-argos3/optimization/expBAG3-2N", 'evo', 'beacagg3-new'),
-            #("/home/khasselmann/neat-argos3/optimization/expBAG3X", 'evo', 'beacagg3x'),
-            # ("/home/khasselmann/neat-argos3/optimization/expBAG2X", 'evo', 'beacagg2x'),
-            # ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'beacagg2.0dev'),
-            # ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'beacagg2.1dev'),
-            # ("/home/khasselmann/neat-argos3/optimization/expBAG", 'evo', 'beacagg')
-        ),
-    "BeacStop": #bstop2.0d-new-exp-03  bstop3.0d-new-exp-01 bstop2.1d-new-exp-01 bstop3.1d-new-exp-01
+    #        ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'bagg6_6_2.1d-240-nlongRange'),
+    #        # ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'beacagg3.0dev'),
+    #        # ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'beacagg3.1dev'),
+    #        # ("/home/khasselmann/neat-argos3/optimization/expBAG3", 'evo', 'beacagg3'),
+    #        # ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'bagg3.0d-new'),
+    #        # ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'bagg2.0d-new'),
+    #        # ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'bagg2.1d-new'),
+    #        # ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'bagg3.1d-new'),
+    #        # ("/home/khasselmann/neat-argos3/optimization/expBAG2-2N", 'evo', 'beacagg2-new'),
+    #        # ("/home/khasselmann/neat-argos3/optimization/expBAG3-2N", 'evo', 'beacagg3-new'),
+    #        #("/home/khasselmann/neat-argos3/optimization/expBAG3X", 'evo', 'beacagg3x'),
+    #        # ("/home/khasselmann/neat-argos3/optimization/expBAG2X", 'evo', 'beacagg2x'),
+    #        # ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'beacagg2.0dev'),
+    #        # ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'beacagg2.1dev'),
+    #        # ("/home/khasselmann/neat-argos3/optimization/expBAG", 'evo', 'beacagg')
+    #    ),
+    #"BeacStop": #bstop2.0d-new-exp-03  bstop3.0d-new-exp-01 bstop2.1d-new-exp-01 bstop3.1d-new-exp-01
+    #    (
+    #        # ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'beacstop3.0dev'),
+    #        # ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'beacstop3.1dev'),
+    #        # ("/home/khasselmann/neat-argos3/optimization/expBSTOP3", 'evo', 'beacstop3'),
+    #        # ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'bstop2.0d-new'),
+    #        # ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'bstop3.0d-new'),
+    #        # ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'bstop2.1d-new'),
+    #        # ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'bstop3.1d-new'),
+    #        # ("/home/khasselmann/neat-argos3/optimization/expBSTOP2-2N", 'evo', 'beacstop2-new'),
+    #        # ("/home/khasselmann/neat-argos3/optimization/expBSTOP3-2N", 'evo', 'beacstop3-new'),
+    #        #("/home/khasselmann/neat-argos3/optimization/expBSTOP3X", 'evo', 'beacstop3x'),
+    #        #("/home/khasselmann/neat-argos3/optimization/expBSTOP2X", 'evo', 'beacstop2x'),
+    #        # ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'beacstop2.0dev'),
+    #        # ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'beacstop2.1dev'),
+    #        # ("/home/khasselmann/neat-argos3/optimization/expBSTOP", 'evo', 'beacstop')
+    #    ),
+    #"BeacDec": # bdec2.0d-new-exp-03  bdec3.0d-new-exp-01 bdec2.1d-new-exp-01 bdec3.1d-new-exp-01
+    #    (
+    #        # ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'beacdec3.0dev'),
+    #        # ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'beacdec3.1dev'),
+    #        #("/home/khasselmann/neat-argos3/optimization/expBDEC3X", 'evo', 'beacdec3x'),
+    #        # ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'bdec2.0d-new'),
+    #        # ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'bdec3.0d-new'),
+    #        # ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'bdec2.1d-new'),
+    #        # ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'bdec3.1d-new'),
+    #        # ("/home/khasselmann/neat-argos3/optimization/expBDEC2-2N", 'evo', 'beacdec2-new'),
+    #        # ("/home/khasselmann/neat-argos3/optimization/expBDEC3-2N", 'evo', 'beacdec3-new'),
+    #        #("/home/khasselmann/neat-argos3/optimization/expBDEC2X", 'evo', 'beacdec2x'),
+    #        # ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'beacdec2.0dev'),
+    #        # ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'beacdec2.1dev2'),
+    #        # ("/home/khasselmann/neat-argos3/optimization/expBDEC", 'evo', 'beacdec')
+    #    )
+    # "xor2":
+    #     (
+    #         ("/home/khasselmann/AutoMoDe-private/optimization", "auto", "xor2_50kdep"),
+    #         ("/home/khasselmann/AutoMoDe-private/optimization", "auto", "xor2_50kNOdep") 
+    #     ),
+    # "foraging":
+    #     (
+    #         ("/home/khasselmann/AutoMoDe-private/optimization", "auto", "foraging_50kdep"),
+    #         ("/home/khasselmann/AutoMoDe-private/optimization", "auto", "foraging_50kNOdep")
+    #     )
+    "shelterblack":
         (
-            # ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'beacstop3.0dev'),
-            # ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'beacstop3.1dev'),
-            # ("/home/khasselmann/neat-argos3/optimization/expBSTOP3", 'evo', 'beacstop3'),
-            # ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'bstop2.0d-new'),
-            # ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'bstop3.0d-new'),
-            # ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'bstop2.1d-new'),
-            # ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'bstop3.1d-new'),
-            # ("/home/khasselmann/neat-argos3/optimization/expBSTOP2-2N", 'evo', 'beacstop2-new'),
-            # ("/home/khasselmann/neat-argos3/optimization/expBSTOP3-2N", 'evo', 'beacstop3-new'),
-            #("/home/khasselmann/neat-argos3/optimization/expBSTOP3X", 'evo', 'beacstop3x'),
-            #("/home/khasselmann/neat-argos3/optimization/expBSTOP2X", 'evo', 'beacstop2x'),
-            # ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'beacstop2.0dev'),
-            # ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'beacstop2.1dev'),
-            # ("/home/khasselmann/neat-argos3/optimization/expBSTOP", 'evo', 'beacstop')
-        ),
-    "BeacDec": # bdec2.0d-new-exp-03  bdec3.0d-new-exp-01 bdec2.1d-new-exp-01 bdec3.1d-new-exp-01
-        (
-            # ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'beacdec3.0dev'),
-            # ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'beacdec3.1dev'),
-            #("/home/khasselmann/neat-argos3/optimization/expBDEC3X", 'evo', 'beacdec3x'),
-            # ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'bdec2.0d-new'),
-            # ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'bdec3.0d-new'),
-            # ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'bdec2.1d-new'),
-            # ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'bdec3.1d-new'),
-            # ("/home/khasselmann/neat-argos3/optimization/expBDEC2-2N", 'evo', 'beacdec2-new'),
-            # ("/home/khasselmann/neat-argos3/optimization/expBDEC3-2N", 'evo', 'beacdec3-new'),
-            #("/home/khasselmann/neat-argos3/optimization/expBDEC2X", 'evo', 'beacdec2x'),
-            # ("/home/khasselmann/argos3-AutoMoDe/optimization", 'auto', 'beacdec2.0dev'),
-            # ("/home/khasselmann/argos3-AutoMoDe21/optimization", 'auto', 'beacdec2.1dev2'),
-            # ("/home/khasselmann/neat-argos3/optimization/expBDEC", 'evo', 'beacdec')
+            ("/home/khasselmann/AutoMoDe-private/optimization", "auto", "shelterblack"),
         )
     }
 
 #METHODS = ('Evostick', 'AutoMoDe-Gianduja', 'AutoMoDe-Chocolate')
+# METHODS = ('AutoMoDe-Chocolate-dep', 'AutoMoDe-Chocolate-NOdep')
+
+METHODS = ('AutoMoDe-Chocolate')
 
 def extract_res_evo(name, remotefolder, sftp, d):
     t = re.compile('{}-(\d+)'.format(name))
@@ -102,7 +119,7 @@ def extract_res_evo(name, remotefolder, sftp, d):
 
 
 def extract_res_auto(name, remotefolder, sftp, d):
-    t = re.compile('{}-exp-(\d\d)'.format(name))
+    t = re.compile('{}-(\d\d)'.format(name))
     m = re.compile(r"# Best configurations as commandlines \(first number is the configuration ID\)\n\d*  (.*)")
     try:
         os.mkdir(d)
